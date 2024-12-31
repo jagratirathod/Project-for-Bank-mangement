8be42298b3e24a2a0a64180412a8efb7b45fe39c
jagrati <jagratir@tech-radix.com>
2024-12-31 17:27:44 +0530

    comment database

--- Bank/settings.py
+++ Bank/settings.py
@@ -84,12 +84,23 @@ WSGI_APPLICATION = 'Bank.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',  # Use MySQL as the database engine
+#         'NAME': 'bank_db',          # Name of your MySQL database
+#         'USER': 'bank_user',             # Your MySQL username
+#         'PASSWORD': 'bank@12345',     # Your MySQL password
+#         'HOST': 'https://bankingsiteproject-emilb25zu-jagratirathods-projects.vercel.app/',       # Usually 'localhost' or an IP address
+#         'PORT': '3306',                        # Default MySQL port
+#     }
+# }
 
 
 # Password validation
